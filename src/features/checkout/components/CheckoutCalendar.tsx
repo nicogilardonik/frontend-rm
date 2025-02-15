6c5915f8adcb51e54bb761033b416d808e05d7c1
Nico Gilardoni <nicogilardonik@gmail.com>
2025-02-15 12:58:13 -0300

    add PrimeReact integration and routing; create CheckoutCalendar component

---
+++ src/features/checkout/components/CheckoutCalendar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "primereact/button";
+
+function CheckoutCalendar() {
+  return (
+    <div>
+      <h1>Mi App con PrimeReact y TypeScript</h1>
+      <Button label="Haz clic" icon="pi pi-check" />
+    </div>
+  );
+}
+
+export default CheckoutCalendar;
