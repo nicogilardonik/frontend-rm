2c2c2962302d697cbfbc48ff55e5fc8d11728a9b
Nico Gilardoni <nicogilardonik@gmail.com>
2025-02-17 12:04:54 -0300

    add product list page and update routing; implement product retrieval logic in CheckoutPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import NotFound from "./core/pages/NotFound";
 import CheckoutPage from "./features/checkout/pages/CheckoutPage";
+import ProductListPage from "./features/product-list/pages/ProductListPage";
 
 function App() {
   return (
     <div className="w-full min-h-screen flex flex-col">
       <Routes>
-        {/* <Route path="/" element={<CheckoutCalendar />} /> */}
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/" element={<ProductListPage />} />
         <Route path="*" element={<NotFound />} />{" "}
       </Routes>
     </div>
--- src/features/checkout/pages/CheckoutPage.tsx
+++ src/features/checkout/pages/CheckoutPage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import CheckoutButtonZone from "../components/CheckoutButton";
 import CheckoutCalendar from "../components/CheckoutCalendar";
 import CheckoutDetails from "../components/CheckoutDetails";
@@ -20,20 +21,43 @@ function CheckoutPage() {
   }>({});
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [searchParams] = useSearchParams();
+  const productId = searchParams.get("productId");
+  const navigate = useNavigate();
 
   useEffect(() => {
+    if (!productId || isNaN(Number(productId))) {
+      navigate("/");
+      return;
+    }
+
     const fetchData = async () => {
       try {
-        const data = await getProduct("123");
+        const data = await getProduct(productId);
         setProduct(data);
       } catch (error) {
-        console.error("Error cargando datos del checkout", error);
+        console.error("Error obteniendo el producto:", error);
       } finally {
         setLoading(false);
       }
     };
+
     fetchData();
-  }, []);
+  }, [productId, navigate]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await getProduct("123");
+  //       setProduct(data);
+  //     } catch (error) {
+  //       console.error("Error cargando datos del checkout", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   const handleDateChange = (range: { from?: Date; to?: Date }) => {
     console.log("Fechas seleccionadas en el padre:", range);
@@ -49,8 +73,26 @@ function CheckoutPage() {
     <div className="min-h-screen bg-gray-50 flex flex-col items-center p-4">
       <div className="w-full max-w-2xl space-y-6">
         {loading ? (
-          <div className="text-center text-gray-600 text-lg font-semibold">
-            ⏳ Cargando producto...
+          <div className="flex flex-col items-center text-gray-600 text-lg font-semibold">
+            <svg
+              className="animate-spin h-6 w-6 text-gray-500"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V4a8 8 0 100 16v-2a8 8 0 01-8-8z"
+              ></path>
+            </svg>
+            <p>⏳ Cargando producto...</p>
           </div>
         ) : product ? (
           <>
